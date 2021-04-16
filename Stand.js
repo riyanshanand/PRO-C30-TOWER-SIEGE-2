26db7d14304e97327b03e28354150d409a820fcc
Riyansh Anand <76739189+riyanshanand@users.noreply.github.com>
2021-04-16 19:12:35 +0530

    Add files via upload

--- Stand.js
+++ Stand.js
@@ -10,11 +10,19 @@ class Stand{
       }
       display(){
         var angle = this.body.angle;
-        push();
-        translate(this.body.position.x, this.body.position.y);
-        rotate(angle);
-        rectMode(CENTER);
-        rect(0,0,this.width, this.height);
-        pop();
+
+        if(this.body.speed < 3){
+          super.display();
+         }
+         else{
+           World.remove(world, this.body);
+           push();
+           this.Visiblity = this.Visiblity - 5;
+           tint(255,this.Visiblity);
+           translate(this.body.position.x, this.body.position.y);
+           rotate(angle);
+           rectMode(CENTER);
+           rect(0,0,this.width, this.height);
+           pop();
+         }
       }
-}
