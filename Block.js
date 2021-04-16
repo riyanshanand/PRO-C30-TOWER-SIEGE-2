7eb05a401b59ee672044a7957d53fe86910a602b
Riyansh Anand <76739189+riyanshanand@users.noreply.github.com>
2021-04-16 22:00:04 +0530

    Add files via upload

--- Block.js
+++ Block.js
@@ -4,14 +4,28 @@ class Block{
           restitution :0.4,
           friction :0.0,
       }
+      this.visiblity = 225;
       this.body = Bodies.rectangle(x, y, width, height, options);
       this.width = width;
       this.height = height;
+      this.image= loadImage("block.png")
       World.add(world, this.body);
     }
     display(){
       var pos= this.body.position;
-      rectMode(CENTER);
-      rect(pos.x,pos.y,this.width, this.height);
+
+      if(this.body.speed < 7){
+        imageMode(CENTER);
+        image(this.image,pos.x,pos.y,this.width, this.height);
+       }
+       else{
+         World.remove(world, this.body);
+         push();
+         this.Visiblity = this.Visiblity - 5;
+         tint(255,this.Visiblity);
+         image(this.image, this.body.position.x, this.body.position.y,this.width, this.height);
+          Pop()
+   
     }
+}
 }
--- Stand.js
+++ Stand.js
@@ -1,28 +1,20 @@
 class Stand{
-    constructor(x, y, width, height) {
-        var options = {
-            isStatic:true
-        }
-        this.body = Bodies.rectangle(x, y, width, height, options);
-        this.width = width;
-        this.height = height;
-        World.add(world, this.body);
+  constructor(x, y, width, height) {
+      var options = {
+          isStatic:true
       }
-      display(){
-        var angle = this.body.angle;
-
-        if(this.body.speed < 3){
-          super.display();
-         }
-         else{
-           World.remove(world, this.body);
-           push();
-           this.Visiblity = this.Visiblity - 5;
-           tint(255,this.Visiblity);
-           translate(this.body.position.x, this.body.position.y);
-           rotate(angle);
-           rectMode(CENTER);
-           rect(0,0,this.width, this.height);
-           pop();
-         }
-      }
+      this.body = Bodies.rectangle(x, y, width, height, options);
+      this.width = width;
+      this.height = height;
+      World.add(world, this.body);
+    }
+    display(){
+      var angle = this.body.angle;
+      push();
+      translate(this.body.position.x, this.body.position.y);
+      rotate(angle);
+      rectMode(CENTER);
+      rect(0,0,this.width, this.height);
+      pop();
+    }
+}
